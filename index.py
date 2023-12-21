d2dd7a05d042e58a06dd707800b8b6b1dc4779f2
AlejandroLMR <alexalejandro4141@gmail.com>
2023-12-21 13:18:57 -0600

    Deploy a Python website
    Start in the web world with a python website wichs is conected with my Main page

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask as fk, render_template as rt
+
+
+# Start Flask
+app = fk(__name__)
+
+# Create a route for create url - its important to create more pages
+@app.route('/')
+# Insert content to index
+def index():
+    return rt('index.html')
+
+@app.route('/about') # Remember do this because home is already use
+def about():
+    return rt('about.html')
+
+# Start the app
+if __name__ == '__main__':
+    app.run(debug=True)
